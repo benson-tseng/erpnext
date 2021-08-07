6603be70bab86fa2cf95cca59b6f0a499f789f2b
benson-tseng <benson010603@gmail.com>
2021-08-07 10:47:05 +0000

    message1

--- erpnext/healthcare/dashboard_chart_source/covid_source/covid_source.py
+++ erpnext/healthcare/dashboard_chart_source/covid_source/covid_source.py
@@ -9,19 +9,29 @@ from frappe.utils.dashboard import cache_source
 @cache_source
 def get(chart_name = None, chart = None, no_cache = None, filters = None, from_date = None,
 	to_date = None, timespan = None, time_interval = None, heatmap_year = None):
-	
+	data=frappe.db.get_list('covid_test',fields=['covid_name','covid_boy','covid_girl'])
+	frappe.logger().debug(data)
 
+	labels = []
+	covid_boy = []
+	covid_girl = []
+
+	for covid in data:
+		labels.append(covid['covid_name'])
+		covid_boy.append(covid['covid_boy'])
+		covid_girl.append(covid['covid_girl'])
+	
 	return {
-		'labels': ["北","中","南","東"],
+		'labels': labels,
 		'datasets': [
 			{
-				'name': '男',
-				'values': [113,152,171,121]
+				'name': 'boy',
+				'values': covid_boy
+				# 'colors':['green']
 			},
 			{
-				'name': '女',
-				'values': [199,153,109,129]
+				'name': 'girl',
+				'values': covid_girl
 			},
-		],
-		'type': 'bar'
+		]
 	}
